fdfb9a82c5e6118d9de1388e3904f0ecd764613f
un-null <movigam.0902@gmail.com>
2022-08-08 11:08:11 +0900

    🎉

---
+++ src/pages/_app.tsx
@@ -0,0 +1,13 @@
+import 'src/libs/tailwind.css'
+import { MantineProvider } from '@mantine/core'
+import type { AppProps } from 'next/app'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <Component {...pageProps} />
+    </MantineProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return <div className="font-bold text-3xl text-blue-500">Hello</div>
+}
+
+export default Home
