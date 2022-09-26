732e0a9d9147566cef425e55650749d9c1ea9e7a
un-null <movigam.0902@gmail.com>
2022-09-26 22:33:11 +0900

    💄

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,12 +29,18 @@ const Home: NextPage<Props> = ({ blogData, portfolioData }) => {
         {/* GitHub Section */}
         <ErrorBoundary
           fallback={
-            <p className="font-bold text-red-400">
+            <p className="mt-24 text-center font-bold text-red-400">
               Failed to fetch Github data.
             </p>
           }
         >
-          <Suspense fallback={<Loader color="pink" />}>
+          <Suspense
+            fallback={
+              <div className="mt-24 flex justify-center">
+                <Loader color="pink" />
+              </div>
+            }
+          >
             <Repositories />
           </Suspense>
         </ErrorBoundary>
@@ -42,12 +48,18 @@ const Home: NextPage<Props> = ({ blogData, portfolioData }) => {
         {/* Twitter Section */}
         <ErrorBoundary
           fallback={
-            <p className="font-bold text-red-400">
+            <p className="mt-24 text-center font-bold text-red-400">
               Failed to fetch Twitter data.
             </p>
           }
         >
-          <Suspense fallback={<Loader color="pink" />}>
+          <Suspense
+            fallback={
+              <div className="mt-24 flex justify-center">
+                <Loader color="pink" />
+              </div>
+            }
+          >
             <Tweets />
           </Suspense>
         </ErrorBoundary>
